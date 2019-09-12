93398ebf4c3bb82d2ae581d9339c5f8c16834c6b
Eli Rose <really.eli@gmail.com>
2019-09-12 01:25:33 -0400

    Assert terminal size.

--- main.py
+++ main.py
@@ -5,7 +5,19 @@ import terminal, render, parser
 TERMINAL_WIDTH = 60
 TERMINAL_HEIGHT = 60
 
+def assert_terminal_size():
+  # Initialize the curses window just to get the terminal size, de-initializing it immediately
+  # afterward. (We use curses.wrapper to do the actual rendering.)
+  max_terminal_height, max_terminal_width = curses.initscr().getmaxyx()
+  curses.endwin()
+
+  if max_terminal_width > TERMINAL_WIDTH:
+    raise Exception(f'Requested terminal width is {TERMINAL_WIDTH}, but the window has width {max_terminal_width}.')
+  if max_terminal_height > TERMINAL_HEIGHT:
+    raise Exception(f'Requested terminal height is {TERMINAL_HEIGHT}, but the window has height {max_terminal_height}.')
+
 if __name__ == "__main__":
+  assert_terminal_size()
   filename = "teapot.obj" if len(sys.argv) <= 1 else sys.argv[1]
 
   with open(filename) as f:
